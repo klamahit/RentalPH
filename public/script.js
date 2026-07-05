7dabc57147260fda582bca758ea02256a96bf03b
OPAC <opac@asiancollege.edu.ph>
2026-07-05 14:59:26 +0800

    Clean remaining rental request localStorage usage

--- public/script.js
+++ public/script.js
@@ -1184,7 +1184,6 @@ function deleteSelectedMessagesForMe() {
 
   messageSelectMode = false;
   selectedMessageIds = [];
-  openConversation(activeChatEmail, false);
 }
 
 function sendChatReply() {
