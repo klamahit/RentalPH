8ac610269f214a511e7b6cc54e6d6d9e443e6b5a
OPAC <opac@asiancollege.edu.ph>
2026-07-05 11:33:36 +0800

    Fix express server initialization and upload limit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 
 const express = require("express");
+const app = express();
+
 const nodemailer = require("nodemailer");
 const cron = require("node-cron");
 const http = require("http");
@@ -60,14 +62,13 @@ function saveItems(items) {
   fs.writeFileSync(itemsFile, JSON.stringify(items, null, 2));
 }
 
-const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 let onlineUsers = {};
 
-app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.static("public"));
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
