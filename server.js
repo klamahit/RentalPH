3a4f742a113af8fbfd9a2b32dc08c1ef09faf15e
OPAC <opac@asiancollege.edu.ph>
2026-07-05 11:02:15 +0800

    Increase upload payload limit

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ const io = new Server(server);
 let onlineUsers = {};
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 const transporter = nodemailer.createTransport({
